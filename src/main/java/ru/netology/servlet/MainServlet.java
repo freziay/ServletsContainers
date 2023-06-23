0a9523921d2dfaa79fad93df896d44905509ee6e
Nastena <freziay@mail.ru>
2023-06-23 14:27:10 +0300

    добавила класс MainServlet.java и удалила класс Main.java

---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,59 @@
+package ru.netology.servlet;
+
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+    private static final String get = "GET";
+    private static final String delete = "DELETE";
+    private static final String post = "POST";
+    private static final String apiPost = "/api/posts";
+    private static final String apiPostD = "/api/posts/\\d+";
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals(get) && path.equals(apiPost)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(get) && path.matches(apiPostD)) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals(post) && path.equals("/api/posts")) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(delete) && path.matches(apiPostD)) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+}

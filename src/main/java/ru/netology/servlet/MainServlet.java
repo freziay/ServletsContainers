e33e1a89b7024352427b143a6eb0bebd19d8151d
Nastena <freziay@mail.ru>
2023-06-28 12:18:20 +0300

    в метод init добавила ApplicationContext

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,7 +1,7 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
@@ -9,52 +9,61 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
-  private PostController controller;
-  private static final String get = "GET";
-  private static final String delete = "DELETE";
-  private static final String post = "POST";
-  private static final String apiPost = "/api/posts";
-  private static final String apiPostD = "/api/posts/\\d+";
+    private PostController controller;
+    private static final String get = "GET";
+    private static final String delete = "DELETE";
+    private static final String post = "POST";
+    private static final String apiPost = "/api/posts";
+    private static final String apiPostD = "/api/posts/\\d+";
 
-  @Override
-  public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
-  }
+    @Override
+    public void init() {
 
-  @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) {
-    // если деплоились в root context, то достаточно этого
-    try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
-      // primitive routing
-      if (method.equals(get) && path.equals(apiPost)) {
-        controller.all(resp);
-        return;
-      }
-      if (method.equals(get) && path.matches(apiPostD)) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
-        controller.getById(id, resp);
-        return;
-      }
-      if (method.equals(post) && path.equals("/api/posts")) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-      if (method.equals(delete) && path.matches(apiPostD)) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
-        controller.removeById(id, resp);
-        return;
-      }
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        // отдаём список пакетов, в которых нужно искать аннотированные классы
+        final var context = new AnnotationConfigApplicationContext("ru.netology");
+
+        // получаем по имени бина
+        final var controller = context.getBean("postController");
+
+        // получаем по классу бина
+        final var service = context.getBean(PostService.class);
+
+        // по умолчанию создаётся лишь один объект на BeanDefinition
+        final var isSame = service == context.getBean("postService");
     }
-  }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals(get) && path.equals(apiPost)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(get) && path.matches(apiPostD)) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals(post) && path.equals("/api/posts")) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(delete) && path.matches(apiPostD)) {
+                // easy way
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
 
